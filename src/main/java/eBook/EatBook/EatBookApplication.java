61f63306307655bbbe9cd7f73ca02d7639d9d1e8
twinogre <glad2you@gmail.com>
2024-02-20 18:10:50 +0900

    init

---
+++ src/main/java/eBook/EatBook/EatBookApplication.java
@@ -0,0 +1,13 @@
+package eBook.EatBook;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EatBookApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EatBookApplication.class, args);
+	}
+
+}
---
+++ src/test/java/eBook/EatBook/EatBookApplicationTests.java
@@ -0,0 +1,13 @@
+package eBook.EatBook;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EatBookApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
