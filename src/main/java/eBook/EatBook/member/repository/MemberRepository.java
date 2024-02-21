22a28c3cbef971d69d7fd66d0d783f7acfde942b
twinogre <glad2you@gmail.com>
2024-02-21 15:26:11 +0900

    member_Entity

---
+++ src/main/java/eBook/EatBook/member/repository/MemberRepository.java
@@ -0,0 +1,9 @@
+package eBook.EatBook.member.repository;
+
+import eBook.EatBook.member.entity.Member;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface MemberRepository extends JpaRepository<Member, Integer> {
+}
