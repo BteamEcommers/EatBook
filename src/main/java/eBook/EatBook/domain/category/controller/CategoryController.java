4d1b4c54566d4c66ff8f95e255d1f078a27e1628
hanseon97 <hanseon97@naver.com>
2024-02-28 11:25:07 +0900

    category

--- src/main/java/eBook/EatBook/domain/category/controller/CategoryController.java
+++ src/main/java/eBook/EatBook/domain/category/controller/CategoryController.java
@@ -8,26 +8,41 @@ import eBook.EatBook.domain.category.service.CategoryService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
 import java.util.List;
 
 @Controller
+@RequiredArgsConstructor
+@RequestMapping("/category")
 public class CategoryController {
-    //여기도 나중에 @Requestmapping("/category")로 바꾸기
-    @GetMapping("/books")
-    public String handleBooksRequest() {
-        // 책 카테고리 선택 페이지로 이동
-        return "books";
-    }
 
-    @GetMapping("/books/category/{category}")
-    public String handleCategoryRequest(@PathVariable("category")String category ) {
-        // 각 카테고리에 해당하는 페이지로 이동
-        return category + "_books";
+    private final CategoryService categoryService;
+
+    @GetMapping("/list")
+    public String handleBooksRequest() {
+       // 책 카테고리 선택 페이지로 이동
+       return "books";
+   }
+@GetMapping("/foreign")
+public String foreign() {
+    // 책 카테고리 선택 페이지로 이동
+    return "foreign_books";
+}
+
+//    @GetMapping("/books/category/{category}")
+//    public String handleCategoryRequest(@PathVariable("category")String category ) {
+//        // 각 카테고리에 해당하는 페이지로 이동
+//        return category + "_books";
+//    }
+
+
+    @PostMapping("/category/foreign")
+    public String foreign(Model model, @RequestParam(value="content") String content) {
+        this.categoryService.foreign(content);
+        // TODO: 답변을 저장한다.
+        return "redirect:/category";
     }
 }
 
--- src/main/java/eBook/EatBook/domain/category/service/CategoryService.java
+++ src/main/java/eBook/EatBook/domain/category/service/CategoryService.java
@@ -1,5 +1,6 @@
 package eBook.EatBook.domain.category.service;
 
+import eBook.EatBook.domain.book.entity.Book;
 import eBook.EatBook.domain.category.entity.Category;
 import eBook.EatBook.domain.category.repository.CategoryRepository;
 import lombok.RequiredArgsConstructor;
@@ -13,5 +14,13 @@ import java.util.Optional;
 public class CategoryService {
     private final CategoryRepository categoryRepository;
 
+    public void foreign(String content) {
+        Category category = Category.builder()
 
+                .content(content)
+
+                .build();
+
+        this.categoryRepository.save(category);
+    }
 }
