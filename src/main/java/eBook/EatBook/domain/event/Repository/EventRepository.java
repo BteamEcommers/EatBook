db9216d4e1eb9226eb75c0c39e408463cddd9fae
kimjeongbae <gprhf651@naver.com>
2024-03-01 11:54:06 +0900

    이벤트 완료

--- src/main/java/eBook/EatBook/domain/event/Repository/EventRepository.java
+++ src/main/java/eBook/EatBook/domain/event/Repository/EventRepository.java
@@ -1,7 +1,10 @@
 package eBook.EatBook.domain.event.Repository;
 
 import eBook.EatBook.domain.event.Entity.Event;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-//public interface EventRepository extends JpaRepository<Event, Integer> {
-//}
+public interface EventRepository extends JpaRepository<Event, Integer> {
+    Page<Event> findAll(Pageable pageable);
+}
