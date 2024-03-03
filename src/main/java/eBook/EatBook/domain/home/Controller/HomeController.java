3a5a0444865f343dbd910c1c685d87bf13270e49
twinogre <glad2you@naver.com>
2024-03-03 19:07:00 +0900

    rebate_page_seller_admin_enhancement

--- src/main/java/eBook/EatBook/domain/home/Controller/HomeController.java
+++ src/main/java/eBook/EatBook/domain/home/Controller/HomeController.java
@@ -1,14 +1,28 @@
 package eBook.EatBook.domain.home.Controller;
 
+import eBook.EatBook.domain.member.entity.Member;
+import eBook.EatBook.domain.member.service.MemberService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import java.security.Principal;
+
 @Controller
+@RequiredArgsConstructor
 public class HomeController {
+    private final MemberService memberService;
 
     @GetMapping("/")
-    public String home() {
+    public String home(Model model, Principal principal) {
+        if (principal != null) {
+            Member member = this.memberService.getMember(principal.getName());
+            model.addAttribute("member", member);
+        }
+
+
 
         return "mainhome";
     }
--- src/main/java/eBook/EatBook/domain/rebate/controller/RebateController.java
+++ src/main/java/eBook/EatBook/domain/rebate/controller/RebateController.java
@@ -3,6 +3,7 @@ package eBook.EatBook.domain.rebate.controller;
 import eBook.EatBook.domain.rebate.service.RebateService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -10,4 +11,16 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @RequestMapping("/rebate")
 public class RebateController {
     private final RebateService rebateService;
+
+    @GetMapping("/admin")
+    public String adminRebateList(){
+
+        return "rebate/rebate_admin";
+    }
+
+    @GetMapping("/seller")
+    public String sellerRebateList(){
+
+        return "rebate/rebate_seller";
+    }
 }
