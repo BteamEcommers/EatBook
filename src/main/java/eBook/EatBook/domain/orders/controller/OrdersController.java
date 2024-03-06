6d4b90128c489633d2e58adee9cdcf19adff6bbe
twinogre <glad2you@naver.com>
2024-03-07 00:58:28 +0900

    주문 생성 구현 /30

--- src/main/java/eBook/EatBook/domain/orders/controller/OrdersController.java
+++ src/main/java/eBook/EatBook/domain/orders/controller/OrdersController.java
@@ -1,23 +1,49 @@
 package eBook.EatBook.domain.orders.controller;
 
+import eBook.EatBook.domain.book.entity.Book;
+import eBook.EatBook.domain.book.service.BookService;
+import eBook.EatBook.domain.member.entity.Member;
+import eBook.EatBook.domain.member.service.MemberService;
+import eBook.EatBook.domain.order_item.entity.OrderItem;
+import eBook.EatBook.domain.order_item.service.OrderItemService;
+import eBook.EatBook.domain.orders.entity.Orders;
 import eBook.EatBook.domain.orders.service.OrdersService;
 import jakarta.persistence.Column;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.security.Principal;
 
 @Controller
 @RequiredArgsConstructor
 @RequestMapping("/orders")
 public class OrdersController {
     private final OrdersService ordersService;
+    private final OrderItemService orderItemService;
+    private final MemberService memberService;
+    private final BookService bookService;
 
-    @GetMapping("/pay")
-    public String ordersPay(){
+    // 단건 결제
+    @GetMapping("/pay/{ordersId}")
+    public String ordersPay(Model model, @PathVariable("ordersId") Integer ordersId, Principal principal){
+        Orders orders = this.ordersService.findById(ordersId);
 
 
         return "/orders/ordersPay";
+    }
+
+    @PostMapping("/pay/{bookId}")
+    public String ordersPayPost(Model model, @PathVariable("bookId") Integer bookId, Principal principal){
+        Book book = this.bookService.getBookById(bookId);
+        Member member = this.memberService.findByUsername(principal.getName());
+        Orders orders = this.ordersService.createOrders(book, member);
+        model.addAttribute("orders", orders);
+        return String.format("redirect:/orders/pay/%d", bookId);
     }
     @GetMapping("/confirm")
     public String ordersConfirm(){
--- src/main/java/eBook/EatBook/domain/orders/service/OrdersService.java
+++ src/main/java/eBook/EatBook/domain/orders/service/OrdersService.java
@@ -1,11 +1,50 @@
 package eBook.EatBook.domain.orders.service;
 
+import eBook.EatBook.domain.book.entity.Book;
+import eBook.EatBook.domain.member.entity.Member;
+import eBook.EatBook.domain.orders.entity.Orders;
 import eBook.EatBook.domain.orders.repository.OrdersRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+
+import java.time.LocalDateTime;
+import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor
 public class OrdersService {
     private final OrdersRepository ordersRepository;
+
+    // 단건 결제
+    public Orders createOrders(Book book, Member member) {
+        Orders orders = Orders.builder()
+                .subject(book.getSubject())
+                .totalPrice(book.getPrice())
+                .book(book)
+                .buyer(member)
+                .createDate(LocalDateTime.now())
+                .modifiedDate(LocalDateTime.now())
+                .build();
+
+        this.ordersRepository.save(orders);
+        return orders;
+    }
+
+    public Orders findByMember(Member member) {
+        Optional<Orders> optionalOrders = this.ordersRepository.findByMember(member);
+        if(optionalOrders.isEmpty()){
+            return null;
+        }
+
+        return optionalOrders.get();
+    }
+
+    public Orders findById(Integer ordersId) {
+        Optional<Orders> optionalOrders = this.ordersRepository.findById(ordersId);
+        if(optionalOrders.isEmpty()){
+            return null;
+        }
+
+        return optionalOrders.get();
+    }
 }
