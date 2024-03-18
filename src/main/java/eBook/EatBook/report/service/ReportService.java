6f1155f9d0517ad688f00c43a4b5af31e10d8e4c
SBS <hanseon97@naver.com>
2024-03-18 17:02:20 +0900

    report_page_delete

--- src/main/java/eBook/EatBook/report/service/ReportService.java
+++ src/main/java/eBook/EatBook/report/service/ReportService.java
@@ -1,12 +1,14 @@
 package eBook.EatBook.report.service;
 
 
+import eBook.EatBook.domain.review.entity.Review;
 import eBook.EatBook.report.Repository.ReportRepository;
 import eBook.EatBook.report.entity.Report;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor
@@ -14,6 +16,17 @@ public class ReportService {
     private final ReportRepository reportRepository;
 
     public List<Report> getReport(){
+
         return this.reportRepository.findAll();
+    }
+    public Report getReportList(Integer id){
+        Optional<Report> report = this.reportRepository.findById(id);
+        if (report.isEmpty()){
+            return null;
+        }
+        return report.get();
+    }
+    public void delete(Report report) {
+        reportRepository.delete(report);
     }
 }
