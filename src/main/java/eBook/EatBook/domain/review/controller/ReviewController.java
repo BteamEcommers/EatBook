fe8def41742ef18d549b7252be8abeb77ab67e01
SBS <hanseon97@naver.com>
2024-03-20 15:44:53 +0900

    star_remodify

--- src/main/java/eBook/EatBook/domain/review/controller/ReviewController.java
+++ src/main/java/eBook/EatBook/domain/review/controller/ReviewController.java
@@ -74,9 +74,9 @@ public class ReviewController {
         }
         Member author = review.getAuthor();
 //         현재 로그인한 사용자가 해당 리뷰의 작성자인지 확인
-        if (!principal.getName().equals(author.getUsername())) {
-            // 작성자와 다른 사용자가 수정을 시도한 경우에 대한 처리
-            return "redirect:/book/list"; // 예시로 책 목록 페이지로 리다이렉트
+        if (!review.getAuthor().getUsername().equals(principal.getName()) &&
+                !principal.getName().equals("admin")) {
+            return "book_detail";
         }
         review.setContent(reviewForm.getContent());
         this.reviewService.modify(review, reviewForm.getContent(),rating);
